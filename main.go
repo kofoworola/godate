a3e975266696e6b9c4c7c900650d6dfbc5026e9f
Kofo Okesola <okesolakofo@gmail.com>
2019-04-30 12:12:02 +0100

    Code update (#3)
    * changed use of constant in godate
    
    * changed use of constants in test

--- main.go
+++ main.go
@@ -6,35 +6,32 @@ import (
 
 var FirstDayOfWeek = time.Monday
 
-//TYPE CONSTANTS
-//TODO Improve this by using the String method
-const (
-	SECONDS = 0x00001
-	MINUTES = 0x00002
-	HOURS   = 0x00003
-	DAYS    = 0x00004
-	WEEKS   = 0x00005
-	MONTHS  = 0x00006
-	YEARS   = 0x00007
-)
+type Unit time.Duration
 
-var UnitStrings = map[int]string{
-	SECONDS: "seconds",
-	MINUTES: "minutes",
-	HOURS:   "hours",
-	DAYS:    "days",
-	WEEKS:   "weeks",
-	MONTHS:  "months",
-	YEARS:   "years",
-}
-
-//UNIT Value CONSTANTS
+//UNIT CONSTANTS
 const (
-	DAY   = time.Hour * 24
+	SECOND Unit = Unit(time.Second)
+	MINUTE = 60 * SECOND
+	HOUR = 60 * MINUTE
+	DAY   = HOUR * 24
 	WEEK  = DAY * 7
 	MONTH = DAY * 30
 	YEAR  = DAY * 365
 )
+
+var UnitStrings = map[Unit]string{
+	SECOND: "seconds",
+	MINUTE: "minutes",
+	HOUR:   "hours",
+	DAY:    "days",
+	WEEK:   "weeks",
+	MONTH:  "months",
+	YEAR:   "years",
+}
+
+func (u Unit) String() string{
+	return UnitStrings[u]
+}
 
 func Create(time time.Time) *GoDate{
 	return &GoDate{time,time.Location()}
@@ -45,11 +42,11 @@ func Now(location *time.Location) *GoDate {
 }
 
 func Tomorrow(location *time.Location) *GoDate {
-	tomorrow := Now(location).Add(1,DAYS)
+	tomorrow := Now(location).Add(1,DAY)
 	return tomorrow
 }
 
 func Yesterday(location *time.Location) *GoDate {
-	yesterday := Now(location).Sub(1,DAYS)
+	yesterday := Now(location).Sub(1,DAY)
 	return yesterday
 }
